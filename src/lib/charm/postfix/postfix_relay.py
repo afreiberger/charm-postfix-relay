03249d6b3b622a9c301655226681312195e8a311
Drew Freiberger <drew.freiberger@canonical.com>
2019-01-08 11:56:37 -0600

    Updated template to use yes/no instead of true/false and added domain_rewrite_map code to config render

--- src/lib/charm/postfix/postfix_relay.py
+++ src/lib/charm/postfix/postfix_relay.py
@@ -16,6 +16,7 @@ from collections import OrderedDict
 from jinja2 import Template
 import os
 import subprocess
+import yaml
 
 from charmhelpers.core.host import (
     install_ca_cert,
@@ -39,6 +40,12 @@ def write_configs():
         f.write(cfg_template.render(PostfixContext()()))
     if config('smtp_auth_username') and config('smtp_auth_password'):
         write_auth_file()
+    if config('domain_rewrite_map'):
+        # create domain rewriting rules
+        filename = '/etc/postfix/smtp_generic_maps.pcre'
+        create_rewrite_map(config["domain_rewrite_map"], filename)
+    restart_postfix()
+
 
 def write_auth_file():
     auth_template_path = os.path.join(TEMPLATES, 'sasl_passwd')
@@ -48,6 +55,23 @@ def write_auth_file():
         f.write(auth_template.render(PostfixContext()()))
     os.chmod(AUTH_CONFIG, 0o600)
     subprocess.check_call(['postmap', 'hash:{}'.format(AUTH_CONFIG)])
+
+
+def generate_rewrite(rewrites):
+    """Produce a pcre-compatible rewrite entry for the domains in the supplied
+    rewrite dictionary.  https://tools.ietf.org/html/rfc5322#section-3.2.3
+    allows a much wider range than we do..."""
+    if rewrites is not None:
+        for domain, newdomain in rewrites:
+            yield '/^([a-z0-9-_.]+)@([a-z][a-z0-9-_]+).' + domain + \
+                '$/  $1+$2@' + newdomain + '\n'
+
+
+def create_rewrite_map(rewrite, filename):
+    """Save the rewrite dictionary to the supplied file."""
+    f = open(filename, 'w')
+    f.writelines(generate_rewrite(yaml.load(rewrite)))
+    f.close()
 
 
 def restart_postfix():
@@ -66,6 +90,18 @@ class PostfixContext():
         ctxt['config'] = config()
         if config('ssl_ca'):
             ctxt['enable_ssl'] = True
+        if config('domain_rewrite_map'):
+            ctxt['have_rewrite_maps'] = True
+        else:
+            ctxt['have_rewrite_maps'] = False
         if config('smtp_auth_username') and config('smtp_auth_password'):
             ctxt['enable_auth'] = True
+        if config('smtpd_helo_required'):
+            ctxt['smtpd_helo_required'] = 'yes'
+        else:
+            ctxt['smtpd_helo_required'] = 'no'
+        if config('disable_vrfy_command'):
+            ctxt['disable_vrfy_command'] = 'yes'
+        else:
+            ctxt['disable_vrfy_command'] = 'no'
         return ctxt
